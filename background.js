ce508b2153e54630ef34c1f8f809545f1db900d6
tyrelt <tyrel.tjoelker@gmail.com>
2015-01-18 11:04:49 -0800

    Add styles for progress bars and links; put basic example values for progress bar in background.js

--- background.js
+++ background.js
@@ -7,5 +7,7 @@ chrome.tabs.query(
       title: "linkCount"
     }, function(count) {
       document.getElementById("linkCount").innerText = count;
+      document.getElementById("kloutMeter").value = 57; // Example values for progress meter
+      document.getElementById("rtMeter").value = 85;
     });
   });
